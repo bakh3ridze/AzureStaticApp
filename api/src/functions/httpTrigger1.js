24437495146f03b0361957042ca453e66a7147e6
giorg22 <giorgibakhturidze2202@outlook.com>
2023-10-05 23:23:48 +0400

    heh1

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -1,5 +1,13 @@
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
+const { app } = require('@azure/functions');
+
+app.http('function1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
