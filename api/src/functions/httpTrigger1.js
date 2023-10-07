b4fdfd0ec9b33b9d464d8f0bf0d3778f88925218
giorg22 <giorgibakhturidze2202@outlook.com>
2023-10-07 17:15:28 +0400

    getClientInfo

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -6,6 +6,22 @@ app.http('function1', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        return { jsonBody: `Hello World!` };
+        // const ip = request.ip;
+        // const userAgent = request.headers['user-agent'];
+
+         const response = await fetch('https://ipinfo.io/json');
+         if (!response.ok) {
+          throw new Error('Network response was not ok');
+         }
+        const data = await response.json();
+
+        const response2 = await fetch('https://api-bdc.net/data/client-info');
+        if (!response2.ok) {
+         throw new Error('Network response was not ok');
+        }
+        const data2 = await response2.json();
+
+
+        return { jsonBody: {data, data2} };
     }
 });
